c9ff32e922df981b982e533e89af899876ef5c81
Simone <garausimone93@ikmail.com>
2024-10-05 09:44:42 +0000

    Gli iperparametri attuali e la correlazine a 0.75 hanno gestito molto bene il precedente overfitting

--- Machine Learning/Logistic Regression With Gradient Descend/main.py
+++ Machine Learning/Logistic Regression With Gradient Descend/main.py
@@ -4,11 +4,11 @@ from validazione import *
 
 if __name__ == "__main__":
     start_time = time.time()
-    plotting = False
+    plotting = True
     param_file_path = 'Assets/best_parameters.json'
     # Carica e pre-processa i dati
     X, y = carica_dati()
-    X_normalized, features_eliminate, y_encoded = preprocessa_dati(X, y, class_balancer="", corr=0.95)
+    X_normalized, features_eliminate, y_encoded = preprocessa_dati(X, y, class_balancer="", corr=0.75)
 
     # Ottieni i nomi delle feature
     all_feature_names = X.columns
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     print(remaining_feature_names)
 
     # Suddivisione in train (80%), test (20%)
-    X_train, X_test, y_train, y_test = train_test_split(X_normalized, y_encoded, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X_normalized, y_encoded, test_size=0.3, random_state=42)
 
     # Caricamento iperparametri
     best_params, best_score = load_best_params()
@@ -80,14 +80,14 @@ if __name__ == "__main__":
     # Plotting
     if plotting:
         plot_learning_curve_with_kfold(
-            model=LogisticRegressionGD(n_iterations=1000),
+            model=LogisticRegressionGD(**best_params),
             X=X_normalized,
             y=y_encoded,
             cv=k,
             model_name=ModelName.LOGISTIC_REGRESSION_GD.value
         )
         plot_learning_curve_with_kfold(
-            model=LogisticRegression(max_iter=1000),
+            model=LogisticRegression(max_iter=best_params.get('n_iterations', 1000)),
             X=X_normalized,
             y=y_encoded,
             cv=k,
